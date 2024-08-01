6b5e960428786e46f1a361bb2f85b3aa6b35d5d2
kunalshelke90 <shelkekunal90@gmail.com>
2024-08-01 19:44:25 +0530

    basic coding  done utils,requirements ,setup ,logging

---
+++ src/mlProject/logging/__init__.py
@@ -0,0 +1,18 @@
+# import os ,sys,logging    ## shifted to mlProjet init constructor for simpler import
+
+
+# logging_str="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+# log_dir="logs"
+
+# log_filepath=os.path.join(log_dir,"running_log.log")
+# os.makedirs(log_dir,exist_ok=True)
+
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format=logging_str,
+#     handlers=[logging.FileHandler(log_filepath),# it gives log in log folder 
+#               logging.StreamHandler(sys.stdout)]#it gives log info on terminal 
+# )
+
+# logger=logging.getLogger("mlProjectLogger")
